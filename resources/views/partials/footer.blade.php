b6258e8d11a3c941542e5cfee8d195204c6ba079
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-10-01 15:55:26 +0200

    [UPD] imported some files from previous repo to stylize the page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer>
+    <div class="foot-up bg-dark-marvel py-5">
+        <div class="container">
+            <div class="row">
+                <div class="d-flex gap-3 mb-2">
+                    <div>
+                        <h4 class="text-white">DC COMICS</h4>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Characters</li>
+                            <li>Comiscs</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h4 class="text-white">SHOP</h4>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectables</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="text-white">DC</h4>
+                        <ul class="text-secondary list-unstyled">
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy(New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>rates</li>
+                            <li>Shop Help</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="text-white">SITES</h4>
+                        <ul class="text-secondary list-unstyled">
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="text-secondary">
+                        All sites Content TM and @ 2020 DC Entertainment, unless otherwise <a href="#">noted
+                            here</a>. All rights reserved. <br>
+                        <a href="#">Cookies Settings</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="foot-down bg-dark">
+        <div class="container py-4 d-flex justify-content-between">
+            <button type="button" class="btn btn-outline-primary text-white">SING-UP NOW!</button>
+            <div class=" d-flex align-items-center gap-3">
+                <span class="text-primary">FOLLOW US</span>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope">
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="upper bg-primary">
+        <div class="container">
+            <ul class="d-flex text-white list-unstyled justify-content-end">
+                <li class="mx-3">
+                    DC POWER VISA@
+                </li>
+                <li>
+                    ADDITIONAL DC SITES
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container d-flex align-items-center justify-content-between">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC LOGO">
+
+            <ul class="list-unstyled d-flex gap-3">
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTABLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+
+            <input type="text">
+        </div>
+    </div>
+</header>
